96bb853a6d403c0f4ebee924e941e11d0d4dce20
David-Jesse <djesse351@gmail.com>
2025-05-03 13:09:47 +0100

    made changes to the authconfig file

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -18,6 +18,17 @@ export const authConfig: NextAuthConfig = {
                 secure: process.env.NODE_ENV === "production"
 
             }
+        },
+        sessionToken: {
+            name: process.env.NODE_ENV === "production"
+            ? "__Secure-next-auth.session-token"
+            : "next-auth.session-token",
+            options: {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+                secure: process.env.NODE_ENV === "production"
+            }    
         }
     },
     callbacks: {
